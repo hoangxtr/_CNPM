fa6565590cf0054e38abd113e6efb0fd885b480e
hoang <xuanhoang232k@gmail.com>
2020-06-29 11:02:48 +0700

    update js and the menu navigator

--- js/1.js
+++ js/1.js
@@ -35,8 +35,7 @@ $(function () {
         $('html').animate({scrollTop: pos}, 1500, "easeOutBack");
     });
     construct();
-    a = JSON.parse(1);
-    console.log(a);
+    show_menu();
     
 });
 
@@ -45,4 +44,53 @@ var about_us = {
     description: "On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word \"and\" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.",
 }
 
+// show menu
+function show_menu() {
+  var rows = document.getElementsByClassName('menu-content')[0];
+  for (var c = 0; c < 2; c ++) {
+    var row = document.createElement('div');
+    row.setAttribute('class', 'row stage');
+    for (var i = 0; i < 4; i ++) {
+      // create col
+      var col = document.createElement("div");
+      col.setAttribute('class', 'col-lg-3 col-sm-5 item');
 
+      // create bouding a tag to col
+      var a_tag = document.createElement("a");
+      setAttributes(a_tag, {
+        'href': "#",
+        'class': 'block w-100'
+      });
+
+      // create img tag
+      img_tag = document.createElement('img');
+      setAttributes(img_tag, {
+        'src': "./assets/food/food1.jpg",
+        'class': 'img-fluid'
+      });
+
+      // create content for the food
+      info_tag = document.createElement("div");
+      info_tag.setAttribute('class', "info");
+
+      h4_tag = document.createElement('h4');
+      h4_tag.innerHTML = 'Title';
+
+      p_tag = document.createElement('p');
+      p_tag.innerHTML = 'On her way she met a copy. The copy warned the Little Blind Text, that where it came';
+
+      info_tag.innerHTML = h4_tag.outerHTML + p_tag.outerHTML;
+      a_tag.innerHTML = img_tag.outerHTML + info_tag.outerHTML;
+      col.appendChild(a_tag);
+      row.appendChild(col);
+    }
+    rows.appendChild(row);
+  }
+  console.log(rows[0])
+}
+
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
